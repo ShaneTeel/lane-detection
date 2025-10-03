3b382d16976db5ed1fc9a2287d4d4a89214c6403
Shane Teel <svteel1106@icloud.com>
2025-10-03 11:04:20 -0400

    Attempted to add drawable canvas for ROI selection; Still needs work

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,10 @@
 import cv2
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+from PIL import Image, ImageDraw
 import requests
 import time
-from functions import initialize_source, clean_up
+from functions import initialize_source
 
 # Page Layout
 st.set_page_config(layout='wide')
@@ -17,10 +19,12 @@ if 'roi_window' not in st.session_state:
     st.session_state['roi_window'] = None
 if 'roi_frame' not in st.session_state:
     st.session_state['roi_frame'] = None
+if 'roi_points' not in st.session_state:
+    st.session_state['roi_points'] = []
+if 'roi_canvas' not in st.session_state:
+    st.session_state['roi_canvas'] = None
 if 'container_lst' not in st.session_state:
     st.session_state['container_lst'] = []
-if 'temp_file_path' not in st.session_state:
-    st.session_state["temp_file_path"] = None
 
 # Set title
 st.title("Traditional Lane Detection Walk-Through")
@@ -41,7 +45,8 @@ with cols0[1]:
 if uploaded_file is not None and uploaded_file != st.session_state['file']:
     st.spinner("Initializing capture object...")
     st.session_state['file'] = uploaded_file
-    cap = initialize_source(st.session_state['file'])
+    video_bytes = uploaded_file.read()
+    cap = initialize_source(video_bytes)
     st.session_state['cap'] = cap
     if not cap.isOpened():
         st.error(f"Failed to read uploaded file, {st.session_state['file'].name}")
@@ -49,58 +54,72 @@ if uploaded_file is not None and uploaded_file != st.session_state['file']:
 if st.session_state['file'] is not None:
 
     st.header("Step 1: Configure Model")
-    cols1 = st.columns(2, border=True)
+    cols1 = st.columns(2)
 
     # ROI Selection
     with cols1[0]:
         st.subheader("Select a Region of Interest* (ROI)")
-        st.markdown("**Move cursor over image and right-click at four different points**")
+        st.markdown("**Move cursor over image and right-click four different points on the image.**")
         st.write("**The ROI is the area of the frame that the Lane Detection model is run against.*")
-
-        # Create ROI Window Session State
+        # Create ROI Session State
+        if st.session_state['roi_frame'] is None:
+            ret, roi_frame = st.session_state['cap'].read()
+            roi_frame_rgb = cv2.cvtColor(roi_frame, cv2.COLOR_BGR2RGB)
+            st.session_state['roi_frame'] = Image.fromarray(roi_frame_rgb)
+        if st.session_state['roi_canvas'] is None:
+            st.session_state['roi_canvas'] = st_canvas(
+                fill_color = "rgba(0, 0, 0, 0)",
+                stroke_width = 2,
+                stroke_color = "rgba(255, 0, 0, 1)",
+                background_image = st.session_state['roi_frame'],
+                width = st.session_state['roi_frame'].width,
+                height = st.session_state['roi_frame'].width,
+                drawing_mode = "polygon",
+                key="canvas"
+            )
         if st.session_state['roi_window'] is None:
             st.session_state['roi_window'] = st.empty()
             st.session_state['container_lst'].append(st.session_state['roi_window'])
-            ret, st.session_state['roi_frame'] = st.session_state['cap'].read()
-        st.session_state['roi_window'].image(st.session_state['roi_frame'], channels='BGR', use_container_width=True)  
+        st.session_state['roi_window'].image(st.session_state['roi_canvas'].image_data)
             
     with cols1[1]:
         st.subheader("Set Parameters")
 
-        # Thresholding Input (cv2.inRange())
-        st.markdown("**Thresholding**")
         cols1A = st.columns(2)
+        
+        # ROI Selection
         with cols1A[0]:
-            lower_bounds = st.number_input("Lower Bounds (Inclusive)", min_value=0, max_value=254, value=150)
+            st.markdown("**Selected ROI**")
+
+        # Thresholding Input (cv2.inRange())
         with cols1A[1]:
-            upper_bounds = st.number_input("Lower Bounds (Inclusive)", min_value=0 if lower_bounds is None else lower_bounds + 1, max_value=255, value=255 if lower_bounds is None else lower_bounds + 1) 
+            st.markdown("**Thresholding**")
+            lower_bounds, upper_bounds = st.slider("Lower / Upper (Inclusive)", min_value=0, max_value=255, value=(150, 255))
 
         # Canny Input (cv2.Canny())
         st.markdown("**Canny Edge Detection**")
-        cols1B = st.columns(3)
+        cols1B = st.columns(2)
         with cols1B[0]:
-            canny_low = st.number_input("Hysteresis Lower Bounds (Inclusive)", min_value=0, max_value=300, value=50)
+            blur_first = st.selectbox("Gaussian Blur", ["Before Canny", "After Canny", "No Blur"], help="Whether to perform Gaussian Blur before edge detection, after edge detection, or not at all.")
         with cols1B[1]:
-            canny_high = st.number_input("Hysteresis Upper Bounds (Inclusive)", min_value=0 if canny_low is None else canny_low + 1, max_value=300, value=100 if canny_low is None else canny_low + 1)
-        with cols1B[2]:
-            blur_first = st.selectbox("Gaussian Blur", ["Before Canny", "After Canny"])
-        
+            canny_low, canny_high = st.slider("Weak / Sure Edge (Inclusive)", min_value=0, max_value=300, value=(50, 150))
+
         # Hough Input (cv2.HoughLinesP())
         st.markdown("**Probabilistic Hough Line Transform**")
         cols1C = st.columns(5)
-        with cols1C[0]:
-            
+        # with cols1C[0]:
+            # rho = st. 
         st.markdown("**Composite Styling***")
 
-        st.write("*Style options do not affect the output of the algorith, only the style in which the lanes are drawn onto the image.")
+        st.write("*Style options do not affect the algorithm.")
 
     view_options = ['Step-by-Step', 'Fully Processed']
     st.divider()
     st.header("Step 2: Inspect & Evaluate")
-    cols2 = st.columns(2, border=True)
+    cols2 = st.columns(2)
     with cols2[0]:
         st.subheader("Visual Inspection")
-        view_selection = st.segmented_control("Render Options", view_options)
+        view_selection = st.selectbox("Render Options", view_options)
         window = st.empty() # Create a placeholder to display frames
         if view_selection == view_options[0]:
             
--- frontend/functions.py
+++ frontend/functions.py
@@ -2,14 +2,9 @@ import os
 import cv2
 import tempfile
 
-def initialize_source(source):
-    video_bytes = source.read()
+def initialize_source(video_bytes):
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as f:
         f.write(video_bytes)
 
     return cv2.VideoCapture(f.name)
 
-def clean_up(cap, container_lst):
-    cap.release()
-    for container in container_lst:
-        container.empty()
